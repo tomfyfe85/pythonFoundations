8645772238ae4903c268eb95b405c7d5a1f7554a
Tom Fyfe <tomfyfe85@gmail.com>
2023-07-07 18:37:45 +0100

    02-Error Messages Are Your Friends

---
+++ Chapter1:PassWord_Validator/02_error_msgs_are_your_friends.py
@@ -0,0 +1,7 @@
+# >>> 1 + 1     # This will work
+# >>> "a" + "b" # This will work
+# >>> 1 + "a"   # This will error
+
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# TypeError: unsupported operand type(s) for +: 'int' and 'str'
