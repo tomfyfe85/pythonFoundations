653a60923e6d472425f135644811e22af485966a
Tom Fyfe <tomfyfe85@gmail.com>
2023-07-07 18:29:01 +0100

    1 say hello to pyhton

--- Chapter1:PassWord_Validator/01_say_hello_to_python.py
+++ Chapter1:PassWord_Validator/01_say_hello_to_python.py
@@ -1,1 +1,29 @@
-# update
+# update; python3
+
+# Python 3.9.13 (main, May 24 2022, 21:28:31) 
+# [Clang 13.1.6 (clang-1316.0.21.2)] on darwin
+# Type "help", "copyright", "credits" or "license" for more information.
+# >>>
+
+# >>> "hello"
+# 'hello'
+
+# >>> "Farewell!"
+# 'Farewell!'
+# >>> "Tis but a scratch."
+# 'Tis but a scratch.'
+# >>> "A scratch? Your arm's off!"
+# "A scratch? Your arm's off!"
+# >>> "I do not know what else to say"
+# 'I do not know what else to say'
+
+# >>> "hello, " + "world!"
+# 'hello, world!'
+
+# >>> greeting = "hello, " + "world!"
+# >>> greeting
+# 'hello, world!'
+
+# >>> name = 'Mina'
+# >>> f"Hello {name}, how are you today?"
+# 'Hello Mina, how are you today?'
