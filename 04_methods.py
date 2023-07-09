1695fcea1464fb213762fc5a318d277bcb48712d
Tom Fyfe <tomfyfe85@gmail.com>
2023-07-09 19:25:44 +0100

    04_methods

---
+++ 04_methods.py
@@ -0,0 +1,17 @@
+# >>> word = "Hello"
+# >>> word.upper()
+# 'HELLO'
+
+# >>> word = "PYTHON"
+# >>> word.lower()
+# 'python'
+# >>> word
+# "PYTHON"
+
+# capitalize
+# lower
+# isascii
+# invert
+# isalpha
+# title
+# strip
