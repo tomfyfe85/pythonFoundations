1785f71f0b553de4087dfb62f8dadac8b8dfab53
Tom Fyfe <tomfyfe85@gmail.com>
2023-07-09 21:03:35 +0100

    06_strings

---
+++ Chapter1:PassWord_Validator/06_beyond_strings.py
@@ -0,0 +1,27 @@
+# >>> # With `and`: to return true, both sides must be true
+# >>> True and True
+# True
+# >>> True and False
+# False
+# >>> False and True
+# False
+
+# >>> # With `or`: to return true, only one side needs to be true
+# >>> True or False
+# True
+# >>> False or True
+# True
+# >>> True or True
+# True
+# >>> False or False
+# False
+
+# >>> # With `not`: returns the opposite of what's given
+# >>> not True
+# False
+# >>> not False
+# True
+
+# >>> password = "..." # Try different values in here
+# >>> len(password) > 7 and len(password) < 13
+# # This will return True or False
