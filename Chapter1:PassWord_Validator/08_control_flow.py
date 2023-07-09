f6de35f165b540c5751b0d14d10a7de3804ea622
Tom Fyfe <tomfyfe85@gmail.com>
2023-07-09 22:59:06 +0100

    08-control_flow

--- Chapter1:PassWord_Validator/08_control_flow.py
+++ Chapter1:PassWord_Validator/08_control_flow.py
@@ -1,11 +1,26 @@
-# # The generic syntax for range is range(start_at, stop_before, step_by
+# # # The generic syntax for range is range(start_at, stop_before, step_by
 
-# >>> name = "Eric"
-# >>> for char in name:
-# ...     print("Give me a " + char + " !") 
-# >>> #???
+# # >>> name = "Eric"
+# # >>> for char in name:
+# # ...     print("Give me a " + char + " !") 
+# # >>> #???
 
-# Give me a E !
-# Give me a r !
-# Give me a i !
-# Give me a c !
+# # Give me a E !
+# # Give me a r !
+# # Give me a i !
+# # Give me a c !
+
+
+# #>>> # This next line imports a function from a Python library.
+# >>> # Don't worry about it too much for now.
+# >>> from random import randint
+# >>> fav_number = 77
+# >>> guess_correct = False
+# >>>
+# >>> while not guess_correct:
+# ...     guess = randint(0, 100)
+# ...     if guess == fav_number:
+# ...         print("You guessed right: 77!")
+# ...         guess_correct = True
+# ...     else:
+# ...         print(f"{guess} is wrong! Try again.")
