1d1308f679d0d3e2cd597afe2921c5304472de25
Tom Fyfe <tomfyfe85@gmail.com>
2023-07-09 22:43:41 +0100

    updates

---
+++ Chapter1:PassWord_Validator/08_control_flow.py
@@ -0,0 +1,11 @@
+# # The generic syntax for range is range(start_at, stop_before, step_by
+
+# >>> name = "Eric"
+# >>> for char in name:
+# ...     print("Give me a " + char + " !") 
+# >>> #???
+
+# Give me a E !
+# Give me a r !
+# Give me a i !
+# Give me a c !
