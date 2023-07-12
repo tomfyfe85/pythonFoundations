51b7de3465729ce76c13ae752cb066cc2114cbd0
Tom Fyfe <tomfyfe85@gmail.com>
2023-07-12 11:24:44 +0100

    12_Review

---
+++ Chapter1:PassWord_Validator/13_Review.py
@@ -0,0 +1,24 @@
+# Name two ways of executing Python code.
+  # In the REPL or by running the file in the terminal - python3 file.py
+
+# How are functions defined?
+  #def function():
+    # code
+    # return code
+
+# When would a method or a function take an argument?
+  # When they need to take data in to perform their tasks
+
+# What is the basic structure of an if statement?
+  # if(condition):
+    #   return True
+    # else:
+    #   return False
+
+# What is refactoring?
+    # Removing unnecessary code and making it more efficient, easy to read, maintain and modify
+
+# What is the difference between for and while loops?
+  # For loops are used when there is a known number of iterations in a sequence,
+  # While loops are used when you want to repeat a block of code until condition is false
+
