cb85d8f1c91fce973ddee0961754870e31b3d104
Tom Fyfe <tomfyfe85@gmail.com>
2023-07-17 22:25:01 +0100

    chapter2 completed

---
+++ Chapter2:PassWord_Manager/07_chapter2_review.py
@@ -0,0 +1,22 @@
+# How do you create and add items to a List?
+var = []
+var.append('stuff')
+
+# How do you create and add items to a Dictionary?
+dict = {}
+dict['key'] = 'val'
+#  => {'key': 'val'}
+
+# List two differences between Lists and Dictionaries
+# 1) Dictionaries are made of key value pairs, where dictionaries are made of individual elements
+# 2) Lists are ordered and each item has an index, Dictionaries are unordered and each element is accessed via it's key.
+
+# How do you define a class?
+# class NewClass():
+
+# What is the difference between a class and an instance of a class?
+# A class is a group of functions designed to process data of the same concern. A blueprint for creating objects.
+# An instance of a class is the class after it has been instantiated and given real data to process
+
+# What is meant by scope?
+# Scope is the availability of code in the program and on what level the data can be accessed IE globally or locally
