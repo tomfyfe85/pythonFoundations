4254152181d1bd56267e7b3eb9d61689ecfad94e
Tom Fyfe <tomfyfe85@gmail.com>
2023-07-20 12:43:03 +0100

    05_advanced_lists upto practice v2

--- Chapter3:PassWord_Manager2.0/05_advanced_lists.py
+++ Chapter3:PassWord_Manager2.0/05_advanced_lists.py
@@ -1,5 +1,5 @@
 passwords = [
-    # {"service": "acebook", "password": "password123", "added_on": "22/03/22"},
+    {'service': 'takeaway', 'password': 'asdf', 'added_on': '21/03/22'},
     {"service": "makersbnb", "password": "qwerty789", "added_on": "22/03/22"},
     {"service": "acebook", "password": "qwsdfdsfsdfsdfsd", "added_on": "22/03/22"},
 ]
@@ -12,3 +12,15 @@ print(next(filter(is_acebook, passwords)))
 
 # using a lambda (anonymous function) instead of the is_acebook function
 print(next(filter(lambda password: password['service'] == 'acebook', passwords)))
+
+# practice v1
+def are_all_passwords_long_enough(passwords):
+    for password in passwords:
+        if len(password['password']) < 8:
+            return False
+    return True
+
+print(are_all_passwords_long_enough(passwords))
+# => false
+
+# practice v2
