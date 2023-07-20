3055e91c5f1a872b10d299027d05cb6eee10a86d
Tom Fyfe <tomfyfe85@gmail.com>
2023-07-20 12:20:28 +0100

    05_advanced_lists added lambda function

--- Chapter3:PassWord_Manager2.0/05_advanced_lists.py
+++ Chapter3:PassWord_Manager2.0/05_advanced_lists.py
@@ -1,12 +1,14 @@
 passwords = [
-    {"service": "acebook", "password": "password123", "added_on": "22/03/22"},
+    # {"service": "acebook", "password": "password123", "added_on": "22/03/22"},
     {"service": "makersbnb", "password": "qwerty789", "added_on": "22/03/22"},
     {"service": "acebook", "password": "qwsdfdsfsdfsdfsd", "added_on": "22/03/22"},
 ]
 
-
+# using the filter method
 def is_acebook(password):
     return password["service"] == "acebook"
 
+print(next(filter(is_acebook, passwords)))
 
-print(list(filter(is_acebook, passwords))[1])
+# using a lambda (anonymous function) instead of the is_acebook function
+print(next(filter(lambda password: password['service'] == 'acebook', passwords)))
