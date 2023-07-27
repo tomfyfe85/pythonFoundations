e731d1c4888fb335c8a7049cd505694b628749fc
Tom Fyfe <tomfyfe85@gmail.com>
2023-07-27 23:46:16 +0100

    addeed greeter() from _2_classes.py

--- Chapter3:PassWord_Manager2.0/scratch.py
+++ Chapter3:PassWord_Manager2.0/scratch.py
@@ -26,7 +26,40 @@ def greater_than(lyst, num):
 print(greater_than([9, 3, 6, 44, 7, 7], 6))
 
 def less_than(lyst, num):
-  newList = filter(lambda int:  int < num, lyst)
+  newList = filter(lambda int: int < num, lyst)
   return list(newList)
 
 print(less_than([9, 3, 6, 44, 1, 7, 7], 6))
+
+def all_above(lyst, num):
+    newList = list(map(lambda int: True if num < int else False, lyst))
+    # return True if True in newList else False
+    return list(newList)
+
+print(all_above([9, 3, 6, 44, 1, 7, 7], 6))
+
+def add_key_value_pair(dic, key, val):
+    dic[key] = val
+    return dic
+
+def where_value_below(dic, num):
+    newDic = {}
+    for key, val in dic.items():
+        if val < num:
+            newDic[key] = val
+    return newDic
+
+print(where_value_below({'cat': 4, 'person': 2, 'centipede': 100}, 5))
+
+class Greeter():
+    def hello(self, name):
+        return f"Hello, {name}!"
+    
+    def goodbye(self, name):
+        return f"Goodbye, {name}!"
+    
+    def good_night(self, name):
+        return f"Good night, {name}!"
+    
+    def good_morning(self, name):
+        return f"Good morning, {name}!"
